0f324212f023b26adea7c042bcbf4891e92261d6
eli <amedzroemmanuel@gmail.com>
2022-11-12 09:12:46 -0500

    Setup server

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("test");
+const express = require("express");
+require("dotenv").config();
+const App = express();
+
+const PORT = process.env.PORT || 5001;
+
+App.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`);
+});
